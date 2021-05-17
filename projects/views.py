47ef0c313dc0eda7421c2807577722358d5f7c34
ryanballar <ballardlawin@gmail.com>
2021-05-17 11:45:56 -0400

    adding tempates update

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from projects.models import Project
  
- def project_index(request):
+def project_index(request):
     projects = Project.objects.all()
     context = {
          'projects': projects
      }
-     return render(request, 'project_index.html', context)
+    return render(request, 'project_index.html', context)
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
